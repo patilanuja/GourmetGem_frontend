9de967ad9dca9c1c01b9b4b1ce5705be61d3cddc
Anuja Patil <patilanuja98@gmail.com>
2024-07-29 22:24:32 -0700

    ui chages

--- src/BaseURL.jsx
+++ src/BaseURL.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000', // Replace with your base URL
+  baseURL: 'http://52.204.247.191:8000', // Replace with your base URL
   timeout: 5000, // Set the timeout for requests (optional)
   headers: { 'Content-Type': 'application/json' }, // Set common headers (optional)
 });
