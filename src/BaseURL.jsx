9b66af3f0f024daad1b84111737ff063e5dbc205
Anuja Patil <42777296+patilanuja@users.noreply.github.com>
2024-08-13 06:31:54 -0700

    Update BaseURL.jsx
    Updated baseurl

--- src/BaseURL.jsx
+++ src/BaseURL.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000', // Replace with your base URL
+  // baseURL: 'http://localhost:8000', // Replace with your base URL
   // baseURL: 'http://load-balancer-344825617.us-east-1.elb.amazonaws.com',
+  baseURL: 'https://anujapatil.pythonanywhere.com/',
   timeout: 5000, // Set the timeout for requests (optional)
   headers: { 'Content-Type': 'application/json' }, // Set common headers (optional)
 });
